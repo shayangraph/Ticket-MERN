d446f09959ec5a85f0531d9c8f6e2f4847107300
shayan jafari <shayanja12345@gmail.com>
2022-12-24 11:32:06 +0330

    add controller to project

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+const registerUser = (req, res) => {
+  res.send("Register Route");
+};
+const loginUser = (req, res) => {
+  res.send("Login Route");
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
