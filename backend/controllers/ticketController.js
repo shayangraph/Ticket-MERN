0ffe394f009afd61454942298896fb331072a67b
shayan jafari <shayanja12345@gmail.com>
2023-01-04 15:53:43 +0330

    create and get ticket backend

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,43 @@ const Ticket = require("../models/ticketModel");
 // @routes GET /api/tickests
 // @access Private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "getTickets" });
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // @desc Create new tickets
 // @routes POST /api/tickests/
 // @access Private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "createTickets" });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error("please add a product and description");
+  }
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: "new",
+  });
+  res.status(201).json(ticket);
 });
 
 module.exports = {
